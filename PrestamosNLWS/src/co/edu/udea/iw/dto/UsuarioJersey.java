112035100c0149069846c040a4faa1703da5c544
vivianalondo <sandrislc@gmail.com>
2017-05-16 03:04:46 -0500

    servicios web de usuario

--- PrestamosNLWS/src/co/edu/udea/iw/dto/UsuarioJersey.java
+++ PrestamosNLWS/src/co/edu/udea/iw/dto/UsuarioJersey.java
@@ -1,10 +1,146 @@
 package co.edu.udea.iw.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /***
  * Clase
- * @author Viviana
+ * @author Viviana Londoño, Johanna Arenas, Oscar Lopera
  *
  */
+
+@XmlRootElement
 public class UsuarioJersey {
 
+	private String identificacion;
+	private String tipoDocumento;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private String email;
+	private String login;
+	private int diasSanciones;
+	private String estadoUsuario;
+	private String rol;
+	
+	public UsuarioJersey() {
+
+	}
+	
+	
+	public UsuarioJersey(String identificacion, String tipoDocumento, String nombre, String apellido, String telefono,
+			String email, String login, int diasSanciones, String estadoUsuario, String rol) {
+		super();
+		this.identificacion = identificacion;
+		this.tipoDocumento = tipoDocumento;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.email = email;
+		this.login = login;
+		this.diasSanciones = diasSanciones;
+		this.estadoUsuario = estadoUsuario;
+		this.rol = rol;
+	}
+
+
+	public String getIdentificacion() {
+		return identificacion;
+	}
+
+
+	public void setIdentificacion(String identificacion) {
+		this.identificacion = identificacion;
+	}
+
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public int getDiasSanciones() {
+		return diasSanciones;
+	}
+
+
+	public void setDiasSanciones(int diasSanciones) {
+		this.diasSanciones = diasSanciones;
+	}
+
+
+	public String getEstadoUsuario() {
+		return estadoUsuario;
+	}
+
+
+	public void setEstadoUsuario(String estadoUsuario) {
+		this.estadoUsuario = estadoUsuario;
+	}
+
+
+	public String getRol() {
+		return rol;
+	}
+
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
 	
 }
---
+++ PrestamosNLWS/src/co/edu/udea/iw/ws/UsuarioWS.java
@@ -0,0 +1,185 @@
+package co.edu.udea.iw.ws;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import co.edu.udea.iw.bl.UsuarioBl;
+import co.edu.udea.iw.dto.Usuario;
+import co.edu.udea.iw.dto.UsuarioJersey;
+import co.edu.udea.iw.exception.MyException;
+
+@Path("Usuario")
+@Component
+public class UsuarioWS {
+
+	@Autowired
+	UsuarioBl usuarioBL;
+	Usuario usuario;
+	
+	/***
+	 * Método para implementar WS de obtener Ususario
+	 * @param identificacion; Recibe el documento de identificacion del Ususario que se desea obtener
+	 * @return usuarioJersey, retorna el usuario obtenido
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("obtener")
+	public UsuarioJersey obtener(@QueryParam("identificacion")String identificacion){
+		UsuarioJersey usuarioJersey = null;
+		
+		try{
+			usuario = usuarioBL.obtener(identificacion);
+			
+			usuarioJersey = new UsuarioJersey(
+					
+					usuario.getIdentificacion(),
+					usuario.getTipoDocumento(),
+					usuario.getNombre(),
+					usuario.getApellido(),
+					usuario.getTelefono(),
+					usuario.getEmail(),
+					usuario.getLogin(),
+					usuario.getDiasSanciones(),
+					usuario.getEstadoUsuario().getTipoEstado(),
+					usuario.getRol().getTipoRol()
+					
+					);
+			
+			
+		}catch(MyException e){
+			e.getMessage();
+		}
+		return usuarioJersey;
+	}
+	
+	/***
+	 * Método que implementa WS para crear un nuevo usuario
+	 * @param identificacion, Documento de identificacion del usuario
+	 * @param tipoDocumento, tipo de documento de identificacion del usuario
+	 * @param nombre, nombre del usuario
+	 * @param apellido, apellido del usuario
+	 * @param telefono, telefono del usuario
+	 * @param email, direccion email del usuario
+	 * @param login, login del usuario
+	 * @param pw, password del usuario
+	 * @param estadoUsuario, id del estado del usuario
+	 * @param rol, id del rol del ususario
+	 * @param loginCrea, el login del usuario que está creando el usuario
+	 * @param pwCrea, la contraseña del usuario que estpa creando el usuario
+	 * @throws RemoteException
+	 * 
+	 *  
+	 */
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Path("guardar")
+	public void guardar(@QueryParam("identificacion")String identificacion, 
+			@QueryParam("tipoDocumento")String tipoDocumento,
+			@QueryParam("nombre")String nombre,
+			@QueryParam("apellido")String apellido, 
+			@QueryParam("telefono")String telefono,
+			@QueryParam("email")String email,
+			@QueryParam("login")String login,
+			@QueryParam("pw")String pw,
+			@QueryParam("estadoUsuario")int estadoUsuario, 
+			@QueryParam("rol")int rol, 
+			@QueryParam("loginCrea")String loginCrea,
+			@QueryParam("pwCrea")String pwCrea) throws RemoteException {
+		try{
+			
+			usuarioBL.registrarUsuario(identificacion, tipoDocumento, nombre, apellido, telefono, email, login, pw, estadoUsuario, rol, loginCrea, pwCrea);
+		}catch(MyException e){
+			throw new RemoteException("Error creando el rol", e);
+		}
+	}
+	
+	/***
+	 * Método que implementa WS para obtener la lista de los usuarios
+	 * @return respuesta, retorna lista con todos los usuarios existetes
+	 * @throws RemoteException
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("listar")
+	public List<UsuarioJersey> listaObtener() throws RemoteException{
+
+		List<UsuarioJersey> respuesta = new ArrayList<UsuarioJersey>();
+		
+		try{
+			for(Usuario usuario: usuarioBL.obtenerUsuarios()){
+				UsuarioJersey usuarioJersey = new UsuarioJersey(
+						usuario.getIdentificacion(),
+						usuario.getTipoDocumento(),
+						usuario.getNombre(),
+						usuario.getApellido(),
+						usuario.getTelefono(),
+						usuario.getEmail(),
+						usuario.getLogin(),
+						usuario.getDiasSanciones(),
+						usuario.getEstadoUsuario().getTipoEstado(),
+						usuario.getRol().getTipoRol()
+						);
+				
+				respuesta.add(usuarioJersey);
+			}
+		}catch(MyException e){
+			throw new RemoteException("Problema consultando los roles", e);
+		}
+		
+		return respuesta;
+	}
+	
+	
+	/***
+	 * Método que implementa WS para modificar usuario
+	 * @param identificacion, Documento de identificacion del usuario
+	 * @param tipoDocumento, tipo de documento de identificacion del usuario
+	 * @param nombre, nombre del usuario
+	 * @param apellido, apellido del usuario
+	 * @param telefono, telefono del usuario
+	 * @param email, direccion email del usuario
+	 * @param login, login del usuario
+	 * @param pw, password del usuario
+	 * @param estadoUsuario, id del estado del usuario
+	 * @param rol, id del rol del ususario
+	 * @param loginCrea, el login del usuario que está creando el usuario
+	 * @param pwCrea, la contraseña del usuario que estpa creando el usuario
+	 * @throws RemoteException
+	 * 
+	 *  
+	 */
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Path("modificar")
+	public void modificar(@QueryParam("identificacion")String identificacion, 
+			@QueryParam("tipoDocumento")String tipoDocumento,
+			@QueryParam("nombre")String nombre,
+			@QueryParam("apellido")String apellido, 
+			@QueryParam("telefono")String telefono,
+			@QueryParam("email")String email,
+			@QueryParam("login")String login,
+			@QueryParam("pw")String pw,
+			@QueryParam("estadoUsuario")int estadoUsuario, 
+			@QueryParam("rol")int rol, 
+			@QueryParam("loginCrea")String loginCrea,
+			@QueryParam("pwCrea")String pwCrea) throws RemoteException {
+		try{
+			usuarioBL.modificarUsuario(identificacion, tipoDocumento, nombre, apellido, telefono, email, login, pw, estadoUsuario, rol, loginCrea, pwCrea);;
+		}catch(MyException e){
+			throw new RemoteException("Error modificando el rol", e);
+		}
+	}
+	
+	
+}
