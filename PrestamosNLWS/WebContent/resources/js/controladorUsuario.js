401b4dfd734a018558c303a3d8e05e21da0512ff
vivianalondo <sandrislc@gmail.com>
2017-06-08 07:43:53 -0500

    editar y eliminar desde el buscar usuario

--- PrestamosNLWS/WebContent/resources/js/controladorUsuario.js
+++ PrestamosNLWS/WebContent/resources/js/controladorUsuario.js
@@ -1,13 +1,16 @@
 /**
- * 
+ * Clase para implementar los controladores y rutas de los usuarios
+ * @author: Viviana Londoño
+ * @version: 1.0
  */
 var moduloUsuarios = angular.module('modUsuarios', ['ngRoute', 'ngCookies', 'neurolab']);
 
 
-
 moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 	
-	//listar 
+	/**
+	 * Función para hacer uso del servicio web listar usuarios
+	 */
 	this.listaUsuarios = function(){
 		return $http({
 			url:'http://localhost:8080/PrestamosNLWS/neurolab/Usuario/listar',
@@ -15,7 +18,9 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 		});
 	}
 	
-	//eliminar	
+	/**
+	 * Función para hacer uso del servicio web eliminar usuarios
+	 */	
 	this.eliminar = function(idUsuario){
 		return $http({
 			url:'http://localhost:8080/PrestamosNLWS/neurolab/Usuario/Eliminar',
@@ -27,7 +32,9 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 	}
 	
 	
-	//buscar
+	/**
+	 * Función para hacer uso del servicio web buscar usuarios
+	 */
 	this.buscar = function(id){
 		return $http({
 			url:'http://localhost:8080/PrestamosNLWS/neurolab/Usuario/obtener',
@@ -39,7 +46,9 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 	}
 	
 	
-	//guardar
+	/**
+	 * Función para hacer uso del servicio web guardar usuarios
+	 */
 	this.guardar = function(user){
 		return $http({
 			url:'http://localhost:8080/PrestamosNLWS/neurolab/Usuario/registrar',
@@ -60,7 +69,9 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 		});
 	}
 
-	//modificar
+	/**
+	 * Función para hacer uso del servicio web modificar usuarios
+	 */
 	this.modificar = function(user){
 		return $http({
 			url:'http://localhost:8080/PrestamosNLWS/neurolab/Usuario/modificar',
@@ -75,7 +86,6 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 						login:user.login,
 						estadoUsuario: user.estadoUsuario,
 						rol:user.rol
-
 			}
 		});
 	}
@@ -85,132 +95,197 @@ moduloUsuarios.service('usuariosServices', function($http, $cookies, $location){
 	
 });
 
-
-moduloUsuarios.controller('usuarioController', function($scope, $rootScope, $location, usuariosServices,productService){
+/**
+ * Controlador de usuarios
+ */
+moduloUsuarios.controller('usuarioController', function($scope, $rootScope, $location, $cookies, usuariosServices, productService, auth){
 	
-	//Lista usuarios
+	$scope.nombreUsuario = $cookies.nombreUsuario;
+	
+	/**
+	 * Función para controlar el listar usuario
+	 */
 	usuariosServices.listaUsuarios().then(
 	function success(data){
-		alert(data.data.usuarioJersey);
 		$scope.listaUsuarios = data.data.usuarioJersey;	
 	});
 	
-	
+	/**
+	 * Función para controlar el buscar usuario
+	 */
+	$scope.buscarusuario = function(identificacion){
+		productService.addProduct(identificacion);
+		$location.url('/buscarusuario');
+	}
 
-	//Función para eliminar usuario
+	/**
+	 * Función para controlar el eliminar usuario
+	 */
 	$scope.eliminar = function(usuario) {
-		alert('acá en eliminar de usuario');
+		
 		usuariosServices.eliminar(usuario.identificacion).then(
 			function success(data){
 				alert('Se ha eliminado el usuario');
 				usuariosServices.listaUsuarios().then(
 						function success(data){
-							alert(data.data.usuarioJersey);
 							$scope.listaUsuarios = data.data.usuarioJersey;	
 						});
 			});
 	};
+	
+	
 
-	//Funciones para editar
+	/**
+	 * Función para controlar el editar usuario
+	 */
 	$scope.editar = function(usuarioM){			
 		$rootScope.usuarioModificar = usuarioM;
 		$location.url('/editarUsuario');
 	};
 	
-	//Función para modificar
+	/**
+	 * Función para controlar el modificar usuario
+	 */
 	$scope.modificaruser = function(usuarioMod) {				
 		usuariosServices.modificar(usuarioMod).then(
 			function success(data){
-				alert('Se ha modificado el dispositivo');
+				alert('Se ha modificado el usuario');
 				$location.url('/listaUsuarios')
 			});
 	};
 	
 	/**
-	//Función eliminar
-	$scope.eliminar = function(dispositivo) {
-		alert('acá en eliminar');
-		dispositivos.eliminar(dispositivo.idDispositivo).then(
+	 * Función para controlar el modificar usuario
+	 */
+	$scope.modificaruserB = function(usuarioMod) {				
+		usuariosServices.modificar(usuarioMod).then(
 			function success(data){
-				alert('Se ha eliminado el dispositivo');
-				$location.url('/listaDispositivos');
-				
+				alert('Se ha modificado el usuario');
+				$location.url('/buscarusuario')
 			});
-		};
-		
-		
-		//Funciones para editar
-		$scope.editar = function(dispositivoM){			
-			$rootScope.dispositivoModificar = dispositivoM;
-			$location.url('/editarDispositivo');
-		};
-		
-		
-		$scope.modificar = function(dispositivoMod) {
-						
-			dispositivos.modificar(dispositivoMod).then(
-				function success(data){
-					alert('Se ha modificado el dispositivo');
-					$location.url('/listaDispositivos')
-				});
-			};
-	*/
-	$scope.buscarusuario = function(identificacion){
-		
-		productService.addProduct(identificacion);
-		$location.url('/buscarusuario');
-	}
+	};
 	
-	//Funcón que me lleva al guardar
+		
+	/**
+	 * Función para controlar el cambio a la vista guardar usuario
+	 */
 	$scope.saveuser = function(){
 		$location.url('/guardarusuario');
 	}
 	
+	/**
+	 * Función para controlar el cambio a la vista listar usuarios
+	 */
+	$scope.backListU = function(){
+		$location.url('/listaUsuarios');
+	}
 	
-	//Función que me lleva al inicio
+	/**
+	 * Función para controlar el cambio a la vista inicio
+	 */
 	$scope.back = function(){
 		$location.url('/inicio');
 	}
+	
+	$scope.listarDispositivos = function(){
+		$location.url('/listaDispositivos');
+	}
+	
+	//Ir al inicio
+	$scope.home = function(){
+		$location.url('/inicio');
+	}
+	
+	//Ir a la lista de usuarios
+	$scope.listarUsuarios = function(){
+		$location.url('/listaUsuarios');
+	}
+	
+	//Ir a la lista de reservas
+	$scope.listarReservas = function(){
+		$location.url('/listaReservas');
+	}
+
+	//Ir a la lista de roles
+	$scope.listarRoles = function(){
+		$location.url('/listaRoles');
+	}
+
+	//Cerrar sesión
+	$scope.logout = function(){
+		auth.logout();
+	}
 });
 
 
-//controlador para buscar usuarios
-moduloUsuarios.controller('busquedausuario', function($scope, $location, usuariosServices,productService){
+/**
+ * Controlador para la búsqueda de usuarios
+ */
+moduloUsuarios.controller('busquedausuario', function($scope, $rootScope, $location, usuariosServices,productService){
 	
 	$scope.products = productService.getProducts();
-	alert($scope.products)
-//	alert("editado")
+
 	usuariosServices.buscar($scope.products).then(
 		function success(data){		
 			$scope.listaUsuariosResultante = data.data;
-		});	
+		});
 	
-	alert($scope.listaUsuariosResultante);
-	
-	
-	//Función que me lleva a la lista dispositivos
-	$scope.backListD = function(){
-		$location.url('/listaUsuarios');
-	}
-});
-
-//Controlador para guardar nuevo usuario
-moduloUsuarios.controller('guardarusuario', function($scope, $location, $cookies, usuariosServices){
-	
-	$scope.nombreUsuario = $cookies.nombreUsuario;
-	$scope.guardaruser = function() {
-		alert('acá en guardar de ususario');
-		usuariosServices.guardar($scope.usuario).then(
+	/**
+	 * Función para eliminar dispositivo desde la vista de búsqueda
+	 */
+	$scope.eliminarB = function(usuario) {
+		usuariosServices.eliminar(usuario.identificacion).then(
 			function success(data){
-				alert('Se ha guardado el usuario');
-				$scope.refrescar = function(){
-					$location.url('/listaUsuarios')
-				};
-				
+				alert('El usuario ha sido eliminado correctamente');
+				usuariosServices.buscar(usuario.identificacion).then(
+						function success(data){	
+							$scope.listaUsuariosResultante = data.data;
+						});	
 			});
 	};
 	
-	//Función que me lleva a la lista dispositivos
+	/**
+	 * Función para editar dispositivo desde la vista de búsqueda
+	 */
+	$scope.editarB = function(usuarioMo){
+		$rootScope.banderaU = 1;
+		$rootScope.usuarioModificar2 = usuarioMo;
+		$location.url('/editarUsuario');
+	};
+	
+	
+
+	
+	/**
+	 * Función para cambiar a la vista listar usuarios
+	 */
+	$scope.backListU = function(){
+		$location.url('/listaUsuarios');
+	}
+});
+
+/**
+ * Controlador para guardar usuario
+ */
+moduloUsuarios.controller('guardarusuario', function($scope, $location, $cookies, usuariosServices){
+	
+	//Variable para guardar el nombre de usuario logueado
+	$scope.nombreUsuario = $cookies.nombreUsuario;
+	
+	/**
+	 * Función para guardar usuario
+	 */
+	$scope.guardaruser = function() {
+		usuariosServices.guardar($scope.usuario).then(
+			function success(data){
+				alert('Se ha guardado el usuario');
+					$location.url('/listaUsuarios')
+			});
+	};
+	
+	/**
+	 * Función para controlar el cambio a la vista listar usuarios
+	 */
 	$scope.backListD = function(){
 		$location.url('/listaUsuarios');
 	}
@@ -219,7 +294,9 @@ moduloUsuarios.controller('guardarusuario', function($scope, $location, $cookies
 
 
 
-//Definir las rutas
+/**
+ * Definición de rutas para el usuario
+ */
 moduloUsuarios.config(['$routeProvider', function($routeProvider){
 	
 	
@@ -242,23 +319,7 @@ moduloUsuarios.config(['$routeProvider', function($routeProvider){
 		templateUrl : 'EditarUsuario.html',
 		controller: 'usuarioController'
 	});
-	
-	
-	
-	
-	
-	/**
-	//Ruta editar
-	$routeProvider.when('/editarDispositivo', {
-		templateUrl : 'EditarDispositivo.html',
-		controller: 'listaDispositivos'
-	});
-	
-	//Ruta editar
-	$routeProvider.when('/eliminarDispositivo', {
-		templateUrl : 'EliminarDispositivos.html',
-		controller: 'eliminarDispositivos'
-	});*/
+
 	
 }]);
 
